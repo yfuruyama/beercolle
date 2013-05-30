be72f758a3d36639e746c085224ae12cf98bf678
Sho Nakatani <lay.sakura@gmail.com>
2013-05-30 12:50:37 +0900

    データ追加 Signed-off-by: Sho Nakatani <lay.sakura@gmail.com>

--- mysql/Beer-DDL.sql
+++ mysql/Beer-DDL.sql
@@ -5,9 +5,11 @@ create table Beer (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(128) NOT NULL,
   style_id INTEGER,
-  ABV DECIMAL(3,1),  -- Alcohol by Volume (e.g. 5.4%)
-  IBU DECIMAL(4,1),  -- IBU(国際苦味単位)
   brewery_id INTEGER,
+  ABV DECIMAL(3,1),  -- Alcohol by Volume (e.g. 5.4%)
+  IBU INTEGER,       -- 国際苦味単位
+  SRM DECIMAL(3,1),  -- 標準参照法．色合いの暗さ．
+  -- OG  DECIMAL(4,3),  -- Original Gravity. 初期比重．
 
   -- Foreign keys
   foreign key(style_id) references BeerStyle(id),
--- mysql/Brewery-DDL.sql
+++ mysql/Brewery-DDL.sql
@@ -1,8 +1,9 @@
 create table Brewery (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(128) NOT NULL,
-  c_id CHAR(2) NOT NULL,   -- Country code
+  country_id CHAR(2) NOT NULL,   -- Country code
+  state VARCHAR(64),             -- 都道府県・州・地名
 
   -- Foreign keys
-  foreign key(c_id) references Country(id)
+  foreign key(country_id) references Country(id)
 );
