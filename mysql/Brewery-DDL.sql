e09d87fdf0d5aea685b2c8c500216de1238d487a
Sho Nakatani <lay.sakura@gmail.com>
2013-06-05 12:25:57 +0900

    - テーブル定義・ロードのSQLを一新．ExcelからエクスポートしたCSVを一発でロードできるようにした． - デフォルトのストレージエンジンをInnoDBに変更． Signed-off-by: Sho Nakatani <lay.sakura@gmail.com>

--- mysql/Brewery-DDL.sql
+++
@@ -1,9 +0,0 @@
-create table Brewery (
-  id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(128) NOT NULL,
-  country_id CHAR(2) NOT NULL,   -- Country code
-  state VARCHAR(64),             -- 都道府県・州・地名
-
-  -- Foreign keys
-  foreign key(country_id) references Country(id)
-);
--- mysql/Country-load.sql
+++ mysql/Country-load.sql
@@ -1,5 +1,11 @@
+create table Country (
+  id CHAR(2) PRIMARY KEY,   -- Country code
+  j_name VARCHAR(64) NOT NULL UNIQUE,
+  e_name VARCHAR(64) NOT NULL UNIQUE  -- English name
+);
+
 load data infile "/home/nakatani/beercolle/git/beercolle/mysql/Country.csv"
   into table Country
   fields terminated by ','
   enclosed by '"'
-  (name, ename, id);
+  (j_name, e_name, id);
