ea9e6c980fa80f8767e6592f3a02b46d97b7f945
Kenta Kase <kesin1202000@gmail.com>
2013-06-05 22:39:47 +0900

    Facebook OAuth認証の移植

--- beer_colle/lib/BeerColle.pm
+++ beer_colle/lib/BeerColle.pm
@@ -3,7 +3,18 @@ use Mojo::Base 'Mojolicious';
 
 # This method will run once at server start
 sub startup {
-  my $self = shift;
+    my $self = shift;
+
+    $self->helper(
+        'facebook' => sub{
+            my $c = shift;
+            my $fb = Facebook::Graph->new(
+                app_id => '131156153751414',
+                secret => '64d3412a6a479ce63c1bab7aa75f58ef',
+                postback => 'http://localhost:3000/callback'
+            );
+        }
+    );
 
   # Documentation browser under "/perldoc"
   $self->plugin('PODRenderer');
@@ -12,7 +23,10 @@ sub startup {
   my $r = $self->routes;
 
   # Normal route to controller
-  $r->get('/')->to('example#welcome');
+  $r->get('/')->to('auth#index');
+  $r->get('/login')->to('auth#login');
+  $r->get('/callback')->to('auth#callback');
+  $r->get('/logout')->to('auth#logout');
 }
 
 1;
