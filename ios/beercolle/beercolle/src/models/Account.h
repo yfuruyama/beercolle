ebfaeb175d8807088171ec5511557cb6016886de
Yuuki Furuyama <addsict@gmail.com>
2013-06-16 19:26:56 +0900

    add Account file

---
+++ ios/beercolle/beercolle/src/models/Account.h
@@ -0,0 +1,15 @@
+//
+//  Account.h
+//  beercolle
+//
+//  Created by Furuyama Yuuki on 6/16/13.
+//  Copyright (c) 2013 Furuyama Yuuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+#import <Accounts/Accounts.h>
+
+@interface Account : NSObject
+
+@end
---
+++ ios/beercolle/beercolle/src/models/Account.m
@@ -0,0 +1,25 @@
+//
+//  Account.m
+//  beercolle
+//
+//  Created by Furuyama Yuuki on 6/16/13.
+//  Copyright (c) 2013 Furuyama Yuuki. All rights reserved.
+//
+
+#import "Account.h"
+
+@implementation Account
+
++ (void)getUserAccount {
+    return [self getTwitterAccount];
+}
+
++ (void)getTwitterAccount {
+    
+}
+
++ (void)getFacebookAccount {
+    
+}
+
+@end
